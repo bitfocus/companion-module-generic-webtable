961864209347ea3bf8dd7f4b5d89127c26780a9c
JaaHann <106606906+JaaHann@users.noreply.github.com>
2024-09-30 20:47:27 +0200

    Fixed issue #7

--- build-config.cjs
+++ build-config.cjs
@@ -2,6 +2,7 @@ module.exports = {
     extraFiles: [
         'webserver-scripts-crypto.js',
         'webserver-scripts-fileManager.js',
+        'webserver-scripts-jsZip.js',
         'webserver-scripts-popupManager.js',
         'webserver-templates-index.html',
     ],
